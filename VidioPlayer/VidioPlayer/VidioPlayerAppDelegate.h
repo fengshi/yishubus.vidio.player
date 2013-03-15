897b00b85cbfa8d068568c6f2ee08ae2e8c79289
shifeng <shifeng007@gmail.com>
2013-03-15 16:39:36 +0800

    no message

---
+++ VidioPlayer/VidioPlayer/VidioPlayerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VidioPlayerAppDelegate.h
+//  VidioPlayer
+//
+//  Created by shi feng on 13-3-15.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VidioPlayerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VidioPlayer/VidioPlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VidioPlayer
+//
+//  Created by shi feng on 13-3-15.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VidioPlayerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VidioPlayerAppDelegate class]));
+    }
+}
