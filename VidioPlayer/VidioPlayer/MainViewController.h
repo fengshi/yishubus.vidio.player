f9bfadaab06860affb604486991ae05953d55c4d
shifeng <shifeng007@gmail.com>
2013-03-18 14:50:05 +0800

    no message

---
+++ VidioPlayer/VidioPlayer/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  VidioPlayer
+//
+//  Created by shi feng on 13-3-18.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ VidioPlayer/VidioPlayer/MainViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainViewController.m
+//  VidioPlayer
+//
+//  Created by shi feng on 13-3-18.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
