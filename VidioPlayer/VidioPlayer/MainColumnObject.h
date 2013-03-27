865d3adc1929769fc75280c095a313abc6d39481
shifeng <shifeng007@gmail.com>
2013-03-27 18:18:05 +0800

    no message

---
+++ VidioPlayer/VidioPlayer/MainColumnObject.h
@@ -0,0 +1,13 @@
+//
+//  MainColumnObject.h
+//  VidioPlayer
+//
+//  Created by shi feng on 13-3-27.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainColumnObject : NSObject
+
+@end
---
+++ VidioPlayer/VidioPlayer/MainColumnObject.m
@@ -0,0 +1,13 @@
+//
+//  MainColumnObject.m
+//  VidioPlayer
+//
+//  Created by shi feng on 13-3-27.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import "MainColumnObject.h"
+
+@implementation MainColumnObject
+
+@end
