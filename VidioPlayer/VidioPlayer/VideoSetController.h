302811a316a7ce4e2feaab0899ab192104643f4d
shifeng <shifeng007@gmail.com>
2013-04-03 17:54:38 +0800

    no message

---
+++ VidioPlayer/VidioPlayer/VideoSetController.h
@@ -0,0 +1,13 @@
+//
+//  VideoSetController.h
+//  VidioPlayer
+//
+//  Created by shi feng on 13-4-3.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoSetController : UIViewController
+
+@end
---
+++ VidioPlayer/VidioPlayer/VideoSetController.m
@@ -0,0 +1,38 @@
+//
+//  VideoSetController.m
+//  VidioPlayer
+//
+//  Created by shi feng on 13-4-3.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import "VideoSetController.h"
+
+@interface VideoSetController ()
+
+@end
+
+@implementation VideoSetController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
