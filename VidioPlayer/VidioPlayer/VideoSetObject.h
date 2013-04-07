d7c204062360e209c8cadb12a35bf6554a426796
shifeng <shifeng007@gmail.com>
2013-04-07 17:42:12 +0800

    no message

---
+++ VidioPlayer/VidioPlayer/VideoSetObject.h
@@ -0,0 +1,13 @@
+//
+//  VideoSetObject.h
+//  VidioPlayer
+//
+//  Created by shi feng on 13-4-7.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VideoSetObject : NSObject
+
+@end
---
+++ VidioPlayer/VidioPlayer/VideoSetObject.m
@@ -0,0 +1,13 @@
+//
+//  VideoSetObject.m
+//  VidioPlayer
+//
+//  Created by shi feng on 13-4-7.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import "VideoSetObject.h"
+
+@implementation VideoSetObject
+
+@end
