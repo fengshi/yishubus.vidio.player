8e9f571beeecd193b1a8f399bee7ca5b4437c7a0
shifeng <shifeng007@gmail.com>
2013-04-09 15:36:42 +0800

    no message

---
+++ VidioPlayer/VidioPlayer/VideoSetCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoSetCell.h
+//  VidioPlayer
+//
+//  Created by shi feng on 13-4-9.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoSetCell : UITableViewCell
+
+@end
