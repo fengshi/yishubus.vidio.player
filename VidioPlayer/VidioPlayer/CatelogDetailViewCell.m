17076f94f89f7904a4de137b7e6abed25c92645b
shifeng <shifeng007@gmail.com>
2013-04-12 17:36:38 +0800

    no message

---
+++ VidioPlayer/VidioPlayer/CatelogDetailViewCell.m
@@ -0,0 +1,29 @@
+//
+//  CatelogDetailViewCell.m
+//  VidioPlayer
+//
+//  Created by shi feng on 13-4-12.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import "CatelogDetailViewCell.h"
+
+@implementation CatelogDetailViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
