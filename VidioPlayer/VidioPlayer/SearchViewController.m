cd27b8b3cfebed2d4a08833b2691231cc1224203
shifeng <shifeng007@gmail.com>
2013-04-17 16:53:22 +0800

    no message

---
+++ VidioPlayer/VidioPlayer/SearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  SearchViewController.m
+//  VidioPlayer
+//
+//  Created by shi feng on 13-4-17.
+//  Copyright (c) 2013年 com.yishubus.vidioplayer. All rights reserved.
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
